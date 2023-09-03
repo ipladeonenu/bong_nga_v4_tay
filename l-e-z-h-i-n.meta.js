e7e70ef730a83e299d6cde21c2b709fc7bfd895c
ipladeonenu <143943040+ipladeonenu@users.noreply.github.com>
2023-09-03 12:42:31 +0000

    cn

---
+++ l-e-z-h-i-n.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         l-e-z-h-i-n
+// @namespace    https://github.com/ipladeonenu/bong_nga_v4_tay/raw/main/l-e-z-h-i-n.user.js
+// @version      2023.09.03
+// @description  down l-e-z-h-i-n
+// @author       You
+// @match        https://www.lezhin.com/ko/comic/*/*
+// @match        https://www.lezhinus.com/en/comic/*/*
+// @match        https://www.lezhin.jp/ja/comic/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.lezhin.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/ipladeonenu/bong_nga_v4_tay/raw/main/l-e-z-h-i-n.user.js
+// @updateURL    https://github.com/ipladeonenu/bong_nga_v4_tay/raw/main/l-e-z-h-i-n.meta.js
+// ==/UserScript==
